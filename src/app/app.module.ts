9d62657bb7a016e08c003e3e1bf48681abdaed17
Kovács <Norbert.Kovacs@kpmg.hu>
2020-04-02 12:30:03 +0200

    Header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import 'hammerjs';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MineSweeperGameComponent } from './mine-sweeper/mine-sweeper-game.component';
-import { MineSweeperMenuComponent } from './mine-sweeper/mine-sweeper-menu/mine-sweeper-menu.component';
-
+import { MatIconModule } from '@angular/material';
+import { MatMenuModule} from '@angular/material/menu';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { MineSweeperGameComponent } from './mine-sweeper/mine-sweeper-game.component';
+import { MineSweeperMenuComponent } from './mine-sweeper/mine-sweeper-menu/mine-sweeper-menu.component';
 import { MineSweeperPopupComponent } from './mine-sweeper/mine-sweeper-popup/mine-sweeper-popup.component';
 import { LoginComponent } from './login/login.component';
 import { GamesComponent } from './games/games.component';
-import { SnakeComponent } from './snake/snake.component'
+import { SnakeComponent } from './snake/snake.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,8 @@ import { SnakeComponent } from './snake/snake.component'
     MineSweeperPopupComponent,
     LoginComponent,
     GamesComponent,
-    SnakeComponent
+    SnakeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +37,9 @@ import { SnakeComponent } from './snake/snake.component'
     MatDialogModule,
     MatButtonModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent],
