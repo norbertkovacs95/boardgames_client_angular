ec0e860e92ad8fdbad33c12bdac5126f6e19b55f
Kovács <Norbert.Kovacs@kpmg.hu>
2020-04-14 12:21:41 +0200

    Login form created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { MatPaginatorModule } from '@angular/material';
 import { MatSortModule } from '@angular/material';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { MineSweeperGameComponent } from './mine-sweeper/mine-sweeper-game.component';
 import { MineSweeperMenuComponent } from './mine-sweeper/mine-sweeper-menu/mine-sweeper-menu.component';
@@ -55,7 +58,11 @@ import { HttpClientModule } from '@angular/common/http';
     MatSortModule,
     MatCardModule,
     HttpClientModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  loginRespError: string;
+  
+  formErrors = {
+    'username': '',
+    'password': ''
+  };
+
+  validationMessages = {
+    'username': {
+      'required':'Email is required.',
+      'email': 'Email must be in correct format'
+    },
+    'password' : {
+      'required':'Password is required.'
+    }
+  };
+
+  constructor(private fb: FormBuilder) { 
+    
+  }
 
   ngOnInit() {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['',[Validators.required]]
+    });
+
+    this.loginForm.valueChanges
+      .subscribe(data => this.onValueChangeLogin(data));
+    this.onValueChangeLogin();
+
+  }
+
+  onValueChangeLogin(data?: any) {
+    if (!this.loginForm) { return; }
+    const form = this.loginForm;
+
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+
+        this.formErrors[field] = '';
+        const control = form.get(field);
+
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+              if (control.errors.hasOwnProperty(key)) {
+                this.formErrors[field] = messages[key];
+                break;
+              }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmitLogin() {
+
   }
 
 }
